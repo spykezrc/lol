ed4e7bd08944895605d5cccc4bc98cb2f8a60bb2
spykezrc <38575062+spykezrc@users.noreply.github.com>
2018-04-21 09:43:23 +0800

    Add files via upload
    First draft.

---
+++ l.c
@@ -0,0 +1,28 @@
+/*
+ * usage: ./l 101010
+ * (c) spykez@yadtech.com
+ * GPL 3 yadda yadda yadda
+ */
+ 
+#include <stdio.h>
+#define L unsigned
+#define O int
+#define lz char
+#define z main
+#define lolz printf
+#define Lz return
+
+L O             l(lz*)       ;L O 
+lO=          0;O    z(O       lOO
+,lz          **      lOl)     {Lz 
+ ((         lOO       ==1     <<1
+)?(         lolz     ("%d     \n", 
+l(          lOl      [1])     )): 
+lolz        ("0       \n"     ));      
+}L O        l(lz      *ll     ){
+Lz(!ll[1])   ?(!(lO=0)&&      ll[0]=='1'):
+(l(ll+1)+(     (ll[0]==      '1')*1<<++lO))
+
+
+                 ;}
+
